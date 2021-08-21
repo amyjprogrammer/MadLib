7024f32ce175279537601a44f03f0e09d8272472
amyjprogrammer <amyjprogrammer@gmail.com>
2021-08-21 19:30:36 -0400

    Adding two methods and a different return method

--- MadLib/Program.cs
+++ MadLib/Program.cs
@@ -6,6 +6,8 @@ namespace MadLib
     {
         static void Main(string[] args)
         {
+            SayHi();
+
             string color, pluralNoun, celebrity;
                 
             Console.Write("Please enter a color: ");
@@ -21,7 +23,26 @@ namespace MadLib
             Console.WriteLine("{0} are blue", pluralNoun);
             Console.WriteLine("I love {0}", celebrity);
 
+            Console.Write("Let's cube a number, please enter the number: ");
+            string num = Console.ReadLine();
+            int cubeNumber = Int32.Parse(num);
+            Console.WriteLine("The cube of {0} is {1}", num, CubeNum(cubeNumber));
+
             Console.ReadLine();
+
         }
+        static void SayHi()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Welcome to Madlib, {0}", name);
+        }
+
+        static int CubeNum(int num)
+        {
+            int result = num * num * num;
+            return result;
+        }
+
     }
 }
