81c181db8988bed0e19c1c7c022059c541da868b
amyjprogrammer <amyjprogrammer@gmail.com>
2021-08-20 22:08:52 -0400

    Add project files.

---
+++ MadLib/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MadLib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string color, pluralNoun, celebrity;
+                
+            Console.Write("Please enter a color: ");
+            color = Console.ReadLine();
+
+            Console.Write("Please enter a plural noun: ");
+            pluralNoun = Console.ReadLine();
+
+            Console.Write("Please enter a celebrity: ");
+            celebrity = Console.ReadLine();
+
+            Console.WriteLine("Roses are {0}", color);
+            Console.WriteLine("{0} are blue", pluralNoun);
+            Console.WriteLine("I love {0}", celebrity);
+
+            Console.ReadLine();
+        }
+    }
+}
